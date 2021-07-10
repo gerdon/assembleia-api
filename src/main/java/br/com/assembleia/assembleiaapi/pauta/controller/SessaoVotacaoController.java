2b0d50e7f9a3152a4bd3f10d78f9bbc61fffb60b
Gerdon Mafra <gerdon_mafra@hotmail.com>
2021-07-10 13:21:34 -0300

    Config initial to Swagger API

--- src/main/java/br/com/assembleia/assembleiaapi/pauta/controller/SessaoVotacaoController.java
+++ src/main/java/br/com/assembleia/assembleiaapi/pauta/controller/SessaoVotacaoController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -21,6 +22,7 @@ import br.com.assembleia.assembleiaapi.pauta.exception.VotoRegistradoException;
 import br.com.assembleia.assembleiaapi.pauta.model.SessaoVotacao;
 import br.com.assembleia.assembleiaapi.pauta.model.Voto;
 import br.com.assembleia.assembleiaapi.pauta.service.SessaoVotacaoService;
+import io.swagger.annotations.Api;
 
 /**
  * @author Gerdon Mafra
@@ -29,6 +31,8 @@ import br.com.assembleia.assembleiaapi.pauta.service.SessaoVotacaoService;
 
 @RestController
 @RequestMapping("sessao-votacao")
+@Api(value = "sessao-votacao")
+@CrossOrigin(origins = "*")
 public class SessaoVotacaoController {
 	
 	@Autowired
